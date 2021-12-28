2f0279eb4bfe949fe6141f3b93fe3057b203658b
August Skare <post@augustskare.no>
2021-12-28 18:53:06 +0100

    v1.0.1

--- mod.ts
+++ mod.ts
@@ -11,7 +11,7 @@ if (import.meta.main) {
   const config = await getConfig(configPath);
 
   const args = yargs(Deno.args).help().scriptName("slm")
-    .version("1.0.0")
+    .version("1.0.1")
     .command(sync)
     .command(assets)
     .command(accounts)
